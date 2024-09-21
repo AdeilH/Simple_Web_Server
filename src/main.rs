f40f7a5e81e67f182ba67f367211526416b3a3c6
adeilh <madeel234@yahoo.com>
2024-09-21 09:45:45 -0400

    Spawning a new Thread at every request

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
-
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        handle_connection(stream);
+
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
-    
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
     // let http_request: Vec<_> = buf_reader
@@ -25,28 +27,25 @@ fn handle_connection(mut stream: TcpStream){
     // .take_while(|line| !line.is_empty())
     // .collect();
 
-    let request_line = buf_reader
-    .lines()
-    .next()
-    .unwrap()
-    .unwrap();
-    println!("{}",request_line);
-
-    let (status_line, file)  = if request_line == "GET / HTTP1.1"{
-        ("HTTP/1.1 200 OK", "hello.html")
-    }else {
-        ("HTTP/1.1 404 NOT FOUND", "error.html")
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("{}", request_line);
+    let (status_line, file) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "error.html"),
     };
 
     let contents = fs::read_to_string(file).unwrap();
     let length = contents.len();
 
     let response = format!(
-            "{status_line}\r\n\
+        "{status_line}\r\n\
             Content-Length: {length}\r\n\r\n\
-            {contents}");
-
+            {contents}"
+    );
 
     stream.write_all(response.as_bytes()).unwrap();
-
 }
